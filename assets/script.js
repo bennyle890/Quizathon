7e3d3de3139b7f3a8a5c00f02c59db0a2a602bd6
Benny Le <benny.le890@gmail.com>
2022-08-10 23:22:16 -0400

    highscores

--- assets/script.js
+++ assets/script.js
@@ -109,7 +109,7 @@ $(document).ready(function () {
         $('.row.centered').empty();
         score = timer.text();
         clearInterval(timeInterval);
-        const formHeader = $('<h3>').text(`Your score is ${score}.`);
+        const formHeader = $('<h3>').text(`Game Over! Your score is ${score}.`);
         const newRow = $('<div>').addClass(['row', 'centered']);
         const form = $('<div>').addClass(['ui', 'form']);
         const field = $('<div>').addClass('field');
@@ -137,11 +137,43 @@ $(document).ready(function () {
             let scoreObj = {score, initials};
             highScores.push(scoreObj);
             localStorage.setItem('highScores', JSON.stringify(highScores));
-            window.location.assign('/scores.html');
+            window.location.assign('/assets/scores.html');
 
         })
     }
 
     startTimer();
+
+});
+
+// High Scores
+$(document).ready(function() {
+    const tableBody = $('#table-body');
+    const clearScoresBtn = $('#clearScores-btn');
+
+    const getScores = () => {
+        let highScores = JSON.parse(localStorage.getItem('highScores'));
+        let sortedScores = highScores.sort(function(a, b) {
+            return b.score - a.score;
+        });
+
+        for (let i = 0; i < sortedScores.length; i++) {
+            const tableRow = $('<tr>');
+            const initialsData = $('<td>').text(highScores[i].initials);
+            const scoreData = $('<td>').text(highScores[i].score);
+            tableBody.append(tableRow);
+            tableRow.append([initialsData, scoreData]);
+
+        }
+
+        clearScoresBtn.on('click', function() {
+            localStorage.clear();
+            location.reload();
+        });
+    }
+
+    
+
+    getScores();
 
 });
